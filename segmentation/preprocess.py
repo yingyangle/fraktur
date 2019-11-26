e00f017cecd305ef18d517c5fc6c346d09ed8ee1
yingyangle <cyang1999@gmail.com>
2019-11-25 21:30:25 -0500

    preprocess.py

---
+++ segmentation/preprocess.py
@@ -0,0 +1,23 @@
+# Christine Yang
+# Fraktur Cracker
+# preprocess.py
+# preprocess an image, making it grayscale and binary
+
+import os, numpy as np, cv2
+
+os.chdir('/Users/Christine/cs/fraktur/segmentation')
+main_dir = os.getcwd()
+
+# takes an image, returns original image, flattened image, and binary grayscale image
+def preprocess(filename):
+    os.chdir(main_dir)
+    img = cv2.imread('test_data/'+filename) # import image
+    nimg = np.array([[x[0] for x in r] for r in img]) # flatten img matrix
+    # preprocess image
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # convert to grayscale
+    ret, thresh = cv2.threshold(img,127,255,cv2.THRESH_BINARY) # convert to binary
+    bin = thresh
+    cv2.imwrite('img.png', img)
+    cv2.imwrite('nimg.png', nimg)
+    cv2.imwrite('bin.png', bin)
+    return (img, nimg, bin)
